d5a101da6f5d23312b4428ec949c361b981850de
Sutharsan.S <78362509+s-sutharsan-20@users.noreply.github.com>
2022-06-23 20:48:38 +0530

    Add files via upload

---
+++ marks representation in dictionery.py
@@ -0,0 +1,15 @@
+marks={
+        'Tamil ':93,
+       'English':84,
+       'Maths':82,
+       'Physics':83,
+       'Chemistry':77,
+       'Computer Science':95
+       }
+print(marks)
+mark=marks.values()
+subject=marks.keys()
+print(subject)
+print(mark)
+a=marks.items()
+print(a)
